29a8a47f588982a620881a2c943b6e0d26da7aff
Pablo Menezes <pmenezes.aju@gmail.com>
2025-03-17 12:48:19 -0300

    Fix listenet redirect https

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -59,16 +59,4 @@ resource "aws_lb_listener" "http_redirect" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Hello, World!"
-      status_code  = "200"
-    }
-  }
-}
+
