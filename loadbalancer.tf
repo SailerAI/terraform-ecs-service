94592b1adda3f68baf150fc3e4f5727f430285e7
Pablo Menezes <pmenezes.aju@gmail.com>
2025-03-17 08:24:41 -0300

    Add listen rule redirect https by default.

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -45,6 +45,20 @@ resource "aws_lb" "main" {
 
 }
 
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
